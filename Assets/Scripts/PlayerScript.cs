0ac248c29d15819fb568d3fe3c67bdff7e8d834c
LD9-TAIKIOSHIDA\ossy3 <ar.tmu.ossy@gmail.com>
2017-04-28 14:18:32 +0900

    プレイヤースクリプトに点数表示を追加

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;  ////ここを追加したよ////
 
 public class PlayerScript : MonoBehaviour {
+
+	public int score = 0; ////ここを追加したよ////
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +14,8 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		this.GetComponent<Text>().text = "点数" + score.ToString() + "点";////ここを追加したよ////
+
 	}
 }
